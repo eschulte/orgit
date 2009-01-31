881dca6503f56023bdf24a4eaad0cd0dbdc3c4d8
Eric Schulte <schulte.eric@gmail.com>
2009-01-31 09:20:14 -0800

    minor tweaks

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,7 +2,7 @@ class Repo < ActiveFile::Base
   self.base_directory = File.join(RAILS_ROOT, "repos")
   self.location = [:name, "/"]
   has_many :pages, :from => :name, :to => :repo_name
-  include ActiveFile::Acts::Git
   
+  acts_as_git
   
 end
